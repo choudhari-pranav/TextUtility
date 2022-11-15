08a8ca3ebe9d988b104e34b09c66c782fb1e89b2
Pranav Choudhari <pranavchoudhari.comp@gmail.com>
2022-11-15 15:51:47 +0530

    Initial Comit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import About from "./components/About";
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+    }
+  };
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 500);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar
+        title="TextUtils"
+        AboutText="About Us"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert alert={alert} />
+      <div className="container my-3"></div> {/* to add space between alert and heading*/}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <TextForm
+                showAlert={showAlert}
+                heading="Enter the text below To Analyze.."
+                mode={mode}
+              />
+            </>
+          }
+        />
+        <Route exact path="/about" element={<About mode={mode} />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleOnChange = (event) => {
+    // console.log("On change");
+    setText(event.target.value);
+  };
+
+  const handleUpClick = () => {
+    // console.log("Uppercase was clicked");
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to uppercase!", "success");
+  };
+
+  const handleLowClick = () => {
+    // console.log("Uppercase was clicked");
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to lowercase!", "success");
+  };
+
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Text Cleared!", "success");
+  };
+  const handleCopy = () => {
+  
+    //The Clipboard interface's writeText() property writes the specified text string to the system clipboard.
+    navigator.clipboard.writeText(text);
+    // document.getSelection().removeAllRanges();
+    props.showAlert("Copied to Clipboard!", "success");
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/); //use RegExp Object A regular expression is a pattern of characters.
+    setText(newText.join(" "));
+    props.showAlert("Extra spaces removed!", "success");
+  };
+
+  const [text, setText] = useState("");
+  // text = "new text"; // Wrong way to change the state
+  // setText("new text"); // Correct way to change the state
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        <h1 className="mb-2"> {props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            style={{
+              backgroundColor: props.mode === "dark" ? "#13466e" : "white",
+              color: props.mode === "dark" ? "white" : "#042743",
+            }}
+            rows="8"
+          ></textarea>
+        </div>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-1
+            "
+          onClick={handleUpClick}
+        >
+          Convert to UpperCase
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-1
+            "
+          onClick={handleLowClick}
+        >
+          Convert to LowerCase
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-1
+            "
+          onClick={handleClearClick}
+        >
+          Clear Text
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-1
+            "
+          onClick={handleExtraSpaces}
+        >
+          Extra Spaces
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-1
+            "
+          onClick={handleCopy}
+        >
+          Copy Text
+        </button>
+      </div>
+
+      <div
+        className="container my-2"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        {/* <h4>Length of text</h4> */}
+        <p>
+          Number of Characters :- {text.length} and Number of words :-
+          {
+            text.split(/\s+/).filter((element) => {
+              //jo array text.split kelayvr milel tyatun blank remove karanar. agar function(filter) true return karrta hai to element rahega nai to remove hoga.
+              // About .filter() 1)The filter() method creates a new array filled with elements that pass a test provided by a function.
+              // 2)The filter() method does not execute the function for empty elements.
+              // 3)The filter() method does not change the original array.*/
+
+              return element.length !== 0;
+              //jis bhi element ke liye true returnkarega vo isme rahega jiske liye false karega vo nai rahega
+            }).length
+          }{" "}
+          in text
+        </p>
+        {/* agara element ki length zero hai to vo element is array me nai rahaega */}
+        <p>
+          Maximum Time to read this text is :-{" "}
+          {0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes to read..
+        </p>
+        <p>
+          <h3>Preview</h3>
+          {text.length > 0 ? text : "Nothing to preview ..."}
+        </p>
+      </div>
+    </>
+  );
+}
